66236aad5c9b5a5a7b454d75cf69b12ccc929077
Matthew C <matthew.chan654@education.nsw.gov.au>
2025-03-10 01:48:01 +0000

    add game

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-
-
+git add -A
+git commit -m "add index"
+git push
---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+player_name = prompt("Enter Your Name");
+alert("Hello " + player_name);
+player_guess = prompt("Guess Rock, Paper Or Scissors");
+computer_guess = randomInteger(1, 3);
+if (player_guess == computer_guess) {
+  document.getElementById("user_feedback").innerHTML =
+    "<em>You Guessed Correct 👍</em>";
+} else {
+  document.getElementById("user_feedback").innerHTML =
+    "<em>Wrong 👎</em><p> You Chose " +
+    player_guess +
+    "!" +
+    "<p> The Answer Was " +
+    computer_guess +
+    "!";
+}
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
